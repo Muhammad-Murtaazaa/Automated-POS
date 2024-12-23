3cf8a1b2586f3217903d8d3289886c4d0ecdc1cd
M-Techs <mmurtaza2300@gmail.com>
2024-12-23 23:11:05 +0500

    made menus

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 using namespace std;
+class menus
+{
+    public:
+        void mainmenu();
+        void inventory();
+};
+void menus::mainmenu()
+{
+    cout << "Main Menu" << endl;
+    cout << "1) Inventory" << endl;
+    cout << "2) Generate Bill" << endl;
+    cout << "4) Show Stock" << endl;
+    cout<<"5) Show sales"<<endl;
+    cout << "3) Exit" << endl;
+}
+void menus::inventory()
+{
+    cout << "Inventory" << endl;
+    cout << "1) Add Item" << endl;
+    cout << "2) Delete Item" << endl;
+    cout << "3) Update Item" << endl;
+    cout << "4) Show Inventory" << endl;
+    cout << "5) Exit" << endl;
+}
 int main() {
-    cout << "Hello, World!" << std::endl;
-    return 0;
+
 }
